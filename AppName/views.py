eedccf51d75448bde9669875236ad92f8e8343ad
Spencer <Spencer.Underwood@Gmail.com>
2015-11-24 22:16:42 -0500

    Change Index, Detail, and Results to generic view

--- AppName/views.py
+++ AppName/views.py
@@ -1,26 +1,43 @@
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
+from django.views import generic
 
 from AppName.models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'AppName/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = RequestContext(request, {'latest_question_list': latest_question_list})
-    return render(request, 'AppName/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'AppName/detail.html'
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "AppName/detail.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'AppName/results.html'
 
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'AppName/results.html', {'question': question})
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = RequestContext(request, {'latest_question_list': latest_question_list})
+#     return render(request, 'AppName/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "AppName/detail.html", {"question": question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'AppName/results.html', {'question': question})
 
 
 def vote(request, question_id):
