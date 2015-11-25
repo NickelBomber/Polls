79b81f43d569fafaa37cdc226074df5b0b87cbf0
Spencer <Spencer.Underwood@Gmail.com>
2015-11-24 21:56:23 -0500

    Initial commit

---
+++ AppName/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Main'             ,               {'fields': ['question_text']}),
+        ('Date information' , {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
+
---
+++ AppName/views.py
@@ -0,0 +1,36 @@
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
+
+from AppName.models import Question, Choice
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = RequestContext(request, {'latest_question_list': latest_question_list})
+    return render(request, 'AppName/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "AppName/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'AppName/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "AppName/detail.html", {"question":p, "error_message":"You didn't select a choice"} )
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('AppName:results', args=(p.id,)))
