ff6c0a5e29fb28ce8adc46c14aad375a2168a289
Spencer Underwood <spencer.underwood@gmail.com>
2015-11-26 10:49:15 -0500

    Add test cases for was_pubished_recently

--- AppName/tests.py
+++ AppName/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
+
+from AppName.models import Question
 # Create your tests here.
+
+class QuestionMethodTewsts(TestCase):
+
+    def test_was_published_recently(self):
+        """
+        esd_publiched_recently() should return False for questions whose pub_date is in the future.
+        :return:
+        """
+        time = timezone.now()+ datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
